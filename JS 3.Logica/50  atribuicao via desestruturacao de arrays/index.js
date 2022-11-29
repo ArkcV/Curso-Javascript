1ef266a83dd9b28071a626fba668276f0605c2b3
ArkcV <andrielmarques@hotmail.com>
2022-11-28 21:13:52 -0300

    Aulas

--- JS 3.Logica/50  atribuicao via desestruturacao de arrays/index.js
+++ JS 3.Logica/50  atribuicao via desestruturacao de arrays/index.js
@@ -2,18 +2,18 @@
 let b = "B"//C
 let c = "C"//A
 
-// isso aquie e atribuicao via desestruturacao [a ,b ,c] , array [1,2,3];
+// isso aqui e atribuicao via desestruturacao [a ,b ,c] , array [1,2,3];
 
 const letras = [b,c,a];
 [a ,b ,c] = letras;
 console.log(a,b,c);*/
 
-/*
-//               0  1  2  3  4  5  6  7  8  
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//                 0  1  2  3  4  5  6  7  8  
+/*const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const [primeiroNumero, segundoNumero, ...resto] = numeros; // operador rest
 console.log(primeiroNumero,segundoNumero);
-console.log(resto);
+console.log(resto);*/
 
 // ...rest = pegar resto // ...spread = espalhar distribuir*/
 
---
+++ JS 3.Logica/52 for classico - estrutura de repeticao /index.js
@@ -0,0 +1,18 @@
+/*xsxfor(let i = 5; i <= 10; i++ ){
+    console.log(i);
+}
+*/
+/*for(let i = 0; i <= 10; i+=1 ){
+    const par =  i % 2 === 0
+    console.log(i,par);
+}*/
+
+//                0      1      2
+const frutas = ['Maça','Pera','Uva','Banana','Limao'];
+
+for (let i = 0 ; i < frutas.length; i++) {
+    console.log(`Indice ${i}`,frutas[i]);
+}
+
+
+/*i - variavel de controle */
