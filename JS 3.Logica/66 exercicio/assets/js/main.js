2862236f9a5fc83346a73145f2bfda447bf59a3a
ArkcV <andrielmarques@hotmail.com>
2022-12-08 14:15:58 -0300

    Aulas

--- JS 3.Logica/66 exercicio/assets/js/main.js
+++ JS 3.Logica/66 exercicio/assets/js/main.js
@@ -6,7 +6,6 @@ const zerar = document.querySelector('.zerar');
 let segundos = 0;
 let timer;
 
-
 function criarSegundos (segundos) {
     const data = new Date(segundos*1000);
     return data.toLocaleTimeString('pt-BR',{
@@ -24,13 +23,18 @@ function iniciarRelogio () {
 document.addEventListener('click',function(e){
     const el = e.target;
 
-
     if (el.classList.contains('iniciar')) {
         console.log('Você clicou em iniciar');
         relogio.classList.remove('pausado');
         clearInterval(timer);
         iniciarRelogio();
-    }
+    };
+
+    if (el.classList.contains('pausar')) {
+        console.log('Você clicou em pausar');
+        clearInterval(timer);  
+        relogio.classList.add('pausado');
+    };
 
     if(el.classList.contains('zerar')) {
         console.log('Você clicou em zera');
@@ -38,12 +42,6 @@ document.addEventListener('click',function(e){
         relogio.innerHTML = "00:00:00";
         segundos = 0;
         relogio.classList.remove('pausado');
-    };
-
-    if (el.classList.contains('pausar')) {
-        console.log('Você clicou em pausar');
-        clearInterval(timer);  
-        relogio.classList.add('pausado');
     };
 
 });
