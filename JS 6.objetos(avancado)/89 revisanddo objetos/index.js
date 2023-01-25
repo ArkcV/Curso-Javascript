f3bd5ca4a8422810a50db8055b8783a7f10c440a
ArkcV <andrielmarques@hotmail.com>
2023-01-25 13:11:47 -0300

    Aulas

---
+++ JS 6.objetos(avancado)/89 revisanddo objetos/index.js
@@ -0,0 +1,27 @@
+
+/*const pessoa = {
+    nome: 'luiz',
+    sobrenome: 'otavio'
+
+};
+const chave= 'nome';
+console.log(pessoa.nome, pessoa.sobrenome);
+//console.log(pessoa['nome'], pessoa['sobrenome']);  console.log(pessoa[chave]);*/
+
+const pessoa1 = new Object();
+pessoa1.nome = 'Luiz';
+pessoa1.sobrenome = 'otavio';
+pessoa1.idade = 30;
+pessoa1.falaNome = function() {
+    return(`${this.nome} esta falando`)
+}
+pessoa1.getDataN = function () {
+    const dataAtual = new Date();
+    return dataAtual.getFullYear() - this.idade;
+}
+
+pessoa1.getDataN();
+console.log(pessoa1.getDataN());
+
+
+/**/
