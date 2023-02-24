95832587060b5fd5ce06ef1f9aa192a3d9eb9750
ArkcV <andrielmarques@hotmail.com>
2023-02-24 14:56:06 -0300

    add : Aulas

---
+++ JS 6.objetos(avancado)/95 heranca/index.js
@@ -0,0 +1,37 @@
+function Produto (nome, preco){
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.aumento = function(quantia) {
+    this.preco += quantia;
+};
+Produto.prototype.desconto = function(quantia) {
+    this.preco -= quantia;
+};
+
+
+
+function Caneca (nome, preco, material) {
+    Produto.call(this,nome, preco)
+    this.material = material;
+}
+
+const caneca = new Caneca ("Caneca", 2.50, "Porcelana");
+console.log(caneca);
+
+
+
+
+function Camiseta (nome, preco, cor) {
+    Produto.call(this,nome, preco)
+    this.cor = cor;
+} 
+
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+
+const camiseta = new Camiseta ('Regata', 15.00, 'Preta');
+camiseta.desconto(10);
+console.log(camiseta);
+
